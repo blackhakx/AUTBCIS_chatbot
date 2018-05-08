0e0259e723ced5e5a98506118374529b2f8cccde
Kenneth <91ken.ken19@gmail.com>
2018-05-08 15:19:53 +1200

    chenged message_handle and added user_intent function

--- chatbot.py
+++ chatbot.py
@@ -20,21 +20,57 @@ def first_entity_value(entities, entity):
 
 
 def major_papers(major):
-    return "major"
+    init_reply = 'That is great! Here is the programme for '
+    if major == 'Analytics':
+        return init_reply + major + ':\nhttps://www.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences/analytics-major'
+    elif major == "Software Development":
+        return init_reply + major + ':\nhttps://autdev.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences/software-development-major'
+    elif major == 'IT Service Sciences':
+        return init_reply + major + ':\nhttps://autdev.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences/it-service-science-major'
+    elif major == 'Computational Intelligence':
+        return init_reply + major + ':\nhttps://autdev.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences/computational-intelligence-major'
+    elif major == 'Network & Security':
+        return init_reply + major + ':\nhttps://autdev.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/courses/bachelor-of-computer-and-information-sciences/networks-and-security-major'
+    elif major is None:
+        return 'What do you want to study?'
+    else:
+        return 'AUT do not offer that programme'
 
 
+def user_intent(entities, intent):
+    # know what user intent
+    if intent == 'study':
+        major = category = first_entity_value(entities, 'major')
+        return major_papers(major)
+
+
+"""
 def handle_message(response):
     entities = response['entities']
-    major = first_entity_value(entities, 'major')
+    #major = first_entity_value(entities, 'major')
     greetings = first_entity_value(entities, 'greetings')
-
+    intent = first_entity_value(entities, 'intent')
     if major:
-        print(major)
         return major_papers(major)
     elif greetings:
         return 'Hello, how can I help you?'
     else:
         return 'I do not understand you'
+"""
+
+
+def handle_message(response):
+    entities = response['entities']
+    intent = first_entity_value(entities, 'intent')
+    greetings = first_entity_value(entities, 'greetings')
+    print(intent, entities)
+    # Check if user intent is defined or wit.ai made entities
+    if intent:
+        return user_intent(entities, intent)
+    elif greetings:
+        return 'Hello, how can I help you?'
+    else:
+        return 'I do not understand you.'
 
 
 client = Wit(access_token=access_token)
