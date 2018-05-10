c9e4da24e623d04064a2813abdeb27e67592fe1c
xrj7445 <xrj7445@gmail.com>
2018-05-10 16:34:18 +1200

    10/5

--- chatbot.py
+++ chatbot.py
@@ -36,13 +36,41 @@ def major_papers(major):
     else:
         return 'AUT do not offer that programme'
 
+def math_paper(major):
+    init_reply = 'Cool! Here are the Mathematics papers for '
+    if major == 'Analytics':
+        return init_reply + major + ', you need to take both STAT500 Applied Statistics and MATH501 Differential & Integral Calculus'
+    elif major == 'Computational Intelligence':
+        return init_reply + major + 'STAT500 Applied Statistics'
+    elif major == 'Computer Science':
+        return init_reply + major + 'MATH502 Algebra & Discrete Mathematics'
+    elif major == 'IT Service Sciences' or 'Network & Security' or 'Software Development':
+        return init_reply + major + 'Choose one from: TAT500 Applied Statistics, MATH500 Mathematical Concepts, MATH501 Differential & Integral Calculus or MATH502 Algebra & Discrete Mathematics'
+    elif major is None:
+        return 'What is your major?'
+    else:
+        return 'AUT does not offer that program'
+
+def programming_paper(paper):
+    if paper == 'Programming 2':
+        return 'Programming 1 is the previous paper required. Have you done Programming 1?'
+    elif paper == 'Programming 1':
+        return 'Sure you can start with Programming 2 next semester.'
+
 
 def user_intent(entities, intent):
     # know what user intent
     if intent == 'study':
         major = category = first_entity_value(entities, 'major')
         return major_papers(major)
-
+    if intent == 'get_math':
+        major = category = first_entity_value(entities, 'major')
+        return math_paper(major)
+    if intent == 'pre_require':
+        paper = category = first_entity_value(entities, 'paper')
+        return programming_paper(paper)
+    if intent == 'elective_paper':
+        return 'Single major students need to choose up to 5 elective papers (75 points) at any level (5,6,7) to make up the 360 points required for the degree. Double major students does not have to take elective papers.'
 
 """
 def handle_message(response):
